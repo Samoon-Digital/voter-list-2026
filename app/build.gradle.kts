3b454e2a4c024b6a670be1b1891670ae9769634d
samun ali <alimailsamun@gmail.com>
2026-09-25 11:07:25 +0530

    versin 25 released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = ProductionPackageName
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "3.3.7"
+        versionCode = 25
+        versionName = "3.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
